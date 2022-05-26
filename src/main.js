c4c7b31cf7420da1c0d5dbf6dc2bd9c7963a90a6
hoozge <hongzhennan@outlook.com>
2022-05-26 14:27:00 +0800

    where?

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(ElementPlus).use(VueAxios, axios).use(router).mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button>this is ep</el-button>
   </div>
 </template>
+<script>
+  import axios from "axios"
+  import { onMounted } from "vue"
+  export default {
+    setup(){
+      onMounted(()=>{
+        axios({
+          url:'http://jsonplaceholder.typicode.com/posts'
+        }).then(res => {
+          console.log(res.data);
+        })
+      })
+    }
+  }
+
+</script>
